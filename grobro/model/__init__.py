d8184161140dce227eb1284b9242bc892ccfe978
criticallimit <fish24@gmx.net>
2026-09-06 17:43:22 +0200

    Expose central device-family API from model package

--- grobro/model/__init__.py
+++ grobro/model/__init__.py
@@ -1,2 +1,13 @@
 from .device_config import DeviceConfig as DeviceConfig
+from .device_family import (
+    DEVICE_FAMILIES as DEVICE_FAMILIES,
+    DeviceFamily as DeviceFamily,
+    get_device_family as get_device_family,
+    get_device_type_name as get_device_type_name,
+    get_known_registers as get_known_registers,
+    is_gateway as is_gateway,
+    is_known_device as is_known_device,
+    supports_time_sync as supports_time_sync,
+    uses_dynamic_pv_count as uses_dynamic_pv_count,
+)
 from .mqtt_config import MQTTConfig as MQTTConfig
