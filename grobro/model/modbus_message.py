7f3b08b550b7c55bb605cbdb3e127fca91cd2661
criticallimit <fish24@gmx.net>
2026-09-06 12:49:42 +0200

    Add fast path for single Modbus register blocks

--- grobro/model/modbus_message.py
+++ grobro/model/modbus_message.py
@@ -176,15 +176,25 @@ class GrowattModbusMessage(BaseModel):
             result += block.size()
         return result
 
+    @staticmethod
+    def _data_from_block(block: GrowattModbusBlock, pos: GrowattRegisterPosition):
+        if block.start > pos.register_no or block.end < pos.register_no:
+            return None
+        block_pos = (pos.register_no - block.start) * 2 + pos.offset
+        end_pos = block_pos + pos.size
+        if block_pos < 0 or end_pos > len(block.values):
+            return None
+        return block.values[block_pos:end_pos]
+
     def get_data(self, pos: GrowattRegisterPosition):
-        for block in self.register_blocks:
-            if block.start > pos.register_no or block.end < pos.register_no:
-                continue
-            block_pos = (pos.register_no - block.start) * 2 + pos.offset
-            end_pos = block_pos + pos.size
-            if block_pos < 0 or end_pos > len(block.values):
-                return None
-            return block.values[block_pos:end_pos]
+        blocks = self.register_blocks
+        if len(blocks) == 1:
+            return self._data_from_block(blocks[0], pos)
+
+        for block in blocks:
+            data = self._data_from_block(block, pos)
+            if data is not None:
+                return data
         return None
 
     @staticmethod
