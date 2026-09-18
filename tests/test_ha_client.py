95629f614aa46b87e34fb114367bead57b496418
criticallimit <fish24@gmx.net>
2026-09-18 08:23:10 +0200

    test: remove obsolete battery warning cache import

--- tests/test_ha_client.py
+++ tests/test_ha_client.py
@@ -17,7 +17,6 @@ from grobro.ha.client import (
     _detect_bat_count,
     _resolve_max_bat,
     _MAX_BAT_CACHE,
-    _LAST_BAT_SERIALS,
 )
 from grobro.model.modbus_message import GrowattModbusFunction, GrowattModbusMessage
 from grobro.model.modbus_function import GrowattModbusFunctionSingle
